b215043f02fcaa5e338019b10291cb651176b41b
Isaac <tadeleyisake@gmail.com>
2022-04-02 15:28:03 -0400

    Initial Commit

---
+++ AccountabilityBuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccountabilityBuddy
+//
+//  Created by Isaac on 4/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
