911ccb4705f6083558351f5c2718943e5aa8ad67
Isaac <tadeleyisake@gmail.com>
2022-04-02 16:21:25 -0400

    Added the settings, connect, goals and forum uiviews with navigation bar

---
+++ AccountabilityBuddy/ConnectViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConnectViewController.swift
+//  AccountabilityBuddy
+//
+//  Created by Isaac on 4/2/22.
+//
+
+import UIKit
+
+class ConnectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
