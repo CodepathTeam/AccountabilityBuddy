61ffa61f5e180732f4ff13d14350c735934ee361
yanelysvelarde <yanelys932@gmail.com>
2022-04-02 17:37:16 -0400

    print hello

--- AccountabilityBuddy/LoginViewController.swift
+++ AccountabilityBuddy/LoginViewController.swift
@@ -11,7 +11,7 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+print ("hello")
         // Do any additional setup after loading the view.
     }
     
