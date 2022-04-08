8d3022a067a8183c74c6d5642ae3f0357ad89939
yanelysvelarde <yanelys932@gmail.com>
2022-04-08 06:18:29 -0400

    Alamofire image

--- AccountabilityBuddy/View/EditProfileViewController.swift
+++ AccountabilityBuddy/View/EditProfileViewController.swift
@@ -7,16 +7,17 @@
 
 import UIKit
 import CameraManager
+import AlamofireImage
 
-class EditProfileViewController: UIViewController,UIImagePickerControllerDelegate {
+class EditProfileViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     @IBOutlet weak var biographyTextField: UITextField!
     
+    @IBOutlet weak var imageSelectorView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         
         
         // Do any additional setup after loading the view.
@@ -31,6 +32,29 @@ class EditProfileViewController: UIViewController,UIImagePickerControllerDelegat
     
     
     @IBAction func onCameraButton(_ sender: Any) {
+       let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        }else {
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        imageSelectorView.layer.borderWidth = 1
+        imageSelectorView.layer.masksToBounds = false
+        imageSelectorView.layer.borderColor = UIColor.black.cgColor
+        imageSelectorView.layer.cornerRadius = imageSelectorView.frame.height/2
+        imageSelectorView.clipsToBounds = true
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageSelectorView.image = scaledImage
+        dismiss(animated: true, completion: nil)
     }
     
     /*
