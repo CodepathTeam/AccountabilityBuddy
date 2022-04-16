2ceba1d35c512bf55bf55fc8ef1e6df2254acd2b
yanelysvelarde <yanelys932@gmail.com>
2022-04-16 00:45:32 -0400

    User can change image

--- AccountabilityBuddy/View/ProfileViewController.swift
+++ AccountabilityBuddy/View/ProfileViewController.swift
@@ -10,18 +10,35 @@
 //import SwiftUI
 import UIKit
 import Parse
+import Foundation
+
 //@available(iOS 15.0, *)
 @IBDesignable
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
-   // var userImage = [PFObject]()
+    var userImage = [PFObject]()
+     
     
     @IBOutlet weak var authorProfilePicture: UIImageView!
     
     @IBOutlet weak var authorLabel: UILabel!
     
     @IBOutlet weak var biographyLabel: UILabel!
+    
+    
+    @IBOutlet var tapEditProfilePicture: UITapGestureRecognizer!
+    
+    
+    
+    
+    
+    var imagePicker:UIImagePickerController!
+    
+    
+    
+    
+    
     
     
     //This function is to make the image round + round margin
@@ -33,14 +50,56 @@ class ProfileViewController: UIViewController {
         authorProfilePicture.layer.borderColor = UIColor.white.cgColor
         authorProfilePicture.layer.cornerRadius = authorProfilePicture.frame.height/2
         authorProfilePicture.clipsToBounds = true
+        
+        
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        
+        
+        let imageTap = UITapGestureRecognizer(target: self, action: #selector(openImagePicker))
+               authorProfilePicture.isUserInteractionEnabled = true
+        authorProfilePicture.addGestureRecognizer(imageTap)
+        authorProfilePicture.layer.cornerRadius = authorProfilePicture.bounds.height / 2
+        authorProfilePicture.clipsToBounds = true
+        tapEditProfilePicture.addTarget(self, action: #selector(getter: authorProfilePicture))
+        
+        
    }
+    @objc func openImagePicker(_ sender:Any) {
+           // Open Image Picker
+        self.present(imagePicker, animated: true, completion: nil)
+        
+       }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        picker.dismiss( animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[String : Any]){
+        
+        if let pickerImage = info[UIImagePickerController.InfoKey.editedImage.rawValue] as? UIImage{
+            
+            authorProfilePicture.image = pickerImage
+            
+            
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
         // Do any additional setup after loading the view.
         
         
     
        // extractView()
 
-}
+
     //GRADIENT BACKGROUND
     
     
