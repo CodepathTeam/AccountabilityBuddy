ed677ebbfa784c0c8e26c56633562159d72b2767
yanelysvelarde <yanelys932@gmail.com>
2022-04-16 02:31:39 -0400

    The user can finally upload a profile picture :D

--- AccountabilityBuddy/View/ProfileViewController.swift
+++ AccountabilityBuddy/View/ProfileViewController.swift
@@ -12,10 +12,11 @@ import UIKit
 import Parse
 import Foundation
 
-//@available(iOS 15.0, *)
+@available(iOS 15.0, *)
 @IBDesignable
 
-class ProfileViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+
+class ProfileViewController: UIViewController {
     
     var userImage = [PFObject]()
      
@@ -27,8 +28,6 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate &
     @IBOutlet weak var biographyLabel: UILabel!
     
     
-    @IBOutlet weak var taptoEditbutton: UIButton!
-    
     
     var imagePicker:UIImagePickerController!
     
@@ -36,32 +35,30 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate &
     
     
     
-    //This function is to make the image round + round margin
+    //This function is to make the image round + round margin+edit image
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       authorProfilePicture.layer.borderWidth = 5
-        authorProfilePicture.layer.masksToBounds = false
-        authorProfilePicture.layer.borderColor = UIColor.white.cgColor
-       authorProfilePicture.layer.cornerRadius = authorProfilePicture.frame.height/2
-       authorProfilePicture.clipsToBounds = true
-        
-        
-        
+    
         imagePicker = UIImagePickerController()
         imagePicker.allowsEditing = true
         imagePicker.sourceType = .photoLibrary
         imagePicker.delegate = self
         
-        
+      
         
         let imageTap = UITapGestureRecognizer(target: self, action: #selector(openImagePicker))
-               authorProfilePicture.isUserInteractionEnabled = true
+        authorProfilePicture.layer.borderWidth = 5
+        authorProfilePicture.layer.masksToBounds = false
+        authorProfilePicture.layer.borderColor = UIColor.white.cgColor
+        authorProfilePicture.layer.cornerRadius = authorProfilePicture.frame.height/2
+        authorProfilePicture.isUserInteractionEnabled = true
         authorProfilePicture.addGestureRecognizer(imageTap)
         authorProfilePicture.layer.cornerRadius = authorProfilePicture.bounds.height / 2
         authorProfilePicture.clipsToBounds = true
-        taptoEditbutton.addTarget(self, action: #selector(self.openImagePicker),
-                                        for: .touchUpInside)
+        
+        //taptoEditbutton.addTarget(self, action: #selector(self.openImagePicker),for: .touchUpInside)
         
         
    }
@@ -72,16 +69,25 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate &
         
         
        }
+}
 
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+
+
+
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss( animated: true, completion: nil)
         
     }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[String : Any]){
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]){
         
-        if let pickerImage = info[UIImagePickerController.InfoKey.editedImage.rawValue] as? UIImage{
-            
-            authorProfilePicture.image = pickerImage
+        if let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+              
+          
+            self.authorProfilePicture.image = pickedImage
             
             
         }
@@ -90,61 +96,3 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate &
     }
     
 }
-
-        // Do any additional setup after loading the view.
-        
-        
-    
-       // extractView()
-
-
-    //GRADIENT BACKGROUND
-    
-    
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //
-  /*  //extracting SwiftUI to UIKit
-    func extractView(){
-        
-            let hostView = UIHostingController(rootView: ProfileHeader())
-        hostView.view.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.view.addSubview(hostView.view)
-        
-        //Constrains...
-        //Clipping
-        let constraints = [
-        
-            hostView.view.topAnchor.constraint(equalTo: view.topAnchor),
-            hostView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            hostView.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            hostView.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
-            //Height and width...
-            hostView.view.widthAnchor.constraint(equalTo: view.widthAnchor),
-            hostView.view.heightAnchor.constraint(equalTo: view.heightAnchor),
-            
-        
-        ]
-        self.view.addConstraints(constraints)
-        
-       
-            // Fallback on earlier versions
-        
-        
-    } */
-    
-    
-    
-
