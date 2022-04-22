1ade60c884a6962c0c2c9b154286af1a0eea60e1
yanelysvelarde <yanelys932@gmail.com>
2022-04-21 23:21:16 -0400

    connections

---
+++ AccountabilityBuddy/View/progressTableView.swift
@@ -0,0 +1,24 @@
+//
+//  progressTableView.swift
+//  AccountabilityBuddy
+//
+//  Created by Yanelys on 4/21/22.
+//
+
+import UIKit
+
+class progressTableView: UITableView {
+
+    
+    @IBOutlet weak var progressTableView: UITableViewCell!
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
